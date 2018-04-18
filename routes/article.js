ba15404ead07b688d990bfd24b48272d0b9209d1
Kat Downes <kathryn.downes@ft.com>
2018-04-18 14:29:01 +0100

    makes successful call to capi

--- routes/article.js
+++ routes/article.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const fetchContent = require('../lib/fetchContent');
 
-router.get('/', (req, res) => {
-  res.render('article');
+router.get('/:uuid', (req, res) => {
+  fetchContent.getArticle(req.params.uuid)
+  .then(content => res.json(content))
+  .catch(err => {
+    res.status(400).send( debug(err) ).end();
+  });
 });
 
 module.exports = router;
