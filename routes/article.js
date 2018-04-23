91f6fd076ac5beb92cb87c7e60946ee3c1647932
Chris Gathercole <chris.gathercole@ft.com>
2018-04-23 11:07:55 +0100

    removed formatting from text

--- routes/article.js
+++ routes/article.js
@@ -9,7 +9,10 @@ router.get('/:uuid', (req, res) => {
   return fetchContent.getArticle(uuid)
   .then(content => {
     const phrases = [];
-    phrases.push({'type': 'Title: ', text: content.title}, {'type': 'Standfirst: ', text: content.standfirst});
+    phrases.push(
+      {'type': 'Title', text: content.title},
+      {'type': 'Standfirst', text: content.standfirst}
+    );
 
     const type = content.annotations.find(object => {
       return object.type === 'GENRE'
@@ -18,7 +21,7 @@ router.get('/:uuid', (req, res) => {
 
     if (type.prefLabel === 'News') {
       const first = text.match(/(^.*?[a-z]{2,}[.!?])\s+\W*[A-Z]/)[1];
-      phrases.push({'type': 'First sentence: ', text: first});
+      phrases.push({'type': 'First sentence', text: first});
     }
 
     return res.render('index', {
