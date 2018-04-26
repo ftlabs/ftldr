fd8dbd4835c31fc3de342faeed180cc277826f5f
Kat Downes <kathryn.downes@ft.com>
2018-04-26 16:33:26 +0100

    tests for annotations

--- test/article.spec.js
+++ test/article.spec.js
@@ -2,7 +2,7 @@ const sinon = require('sinon');
 const chai = require('chai');
 const expect = require('chai').expect;
 const proxyquire = require('proxyquire');
-const fixture = require('./fixtures/capiResponse.json');
+const newsFixture = require('./fixtures/newsResponse.json');
 
 const sandbox = sinon.sandbox.create();
 
@@ -15,15 +15,17 @@ const subject = proxyquire('../lib/article', {'../lib/fetchContent': capiStub});
 
 describe('article', () => {
 
-	beforeEach(() => {
-		capiStub.getArticle.returns(Promise.resolve(fixture));
-	});
-
 	afterEach(() => {
 		sandbox.reset();
 	});
 
 	const id = '70fc3c0e-1cb5-11e8-956a-43db76e69936';
+
+	context('content requested is a news article', () => {
+
+		beforeEach(() => {
+			capiStub.getArticle.returns(Promise.resolve(newsFixture));
+		});
 
 	it('gets the title from the capi response', () => {
 		return subject.extractData(id)
@@ -31,7 +33,7 @@ describe('article', () => {
 			expect(res.essencePhrases[0]).to.include({
 				"type": "Title",
 	      "text": "PSA chief demands fairness on emissions penalties"
-			})
+			});
 		});
 	});
 
@@ -41,8 +43,18 @@ describe('article', () => {
 			expect(res.essencePhrases[1]).to.include({
 				"type": 'Standfirst',
     		"text": 'Carlos Tavares says countries must install enough electric-car charging points'
+			});
+		});
+	});
+
+	it('gets each predicate and type from the annotations', () => {
+		return subject.extractData(id)
+		.then(res => {
+			expect(res.context[1]).to.include({
+				"type": 'annotation:about:ORGANISATION', text: 'PSA Group'
 			})
 		});
 	});
+});
 
 });
